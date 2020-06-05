f8243d72b41a14c0f03abaefd6d4fd934ae42a02
philipDmartin <philipnathan788@gmail.com>
2020-06-05 16:29:57 -0500

    added if to program

--- Program.cs
+++ Program.cs
@@ -43,6 +43,64 @@ namespace Bank_Heist_5
                 SkillLevel = 40,
                 PercentageCut = 20
             };
+
+            rolodex.Add(Tyler);
+
+            while (true)
+            {
+                Console.WriteLine("Choose robber name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("choose specialty");
+                Console.WriteLine("1.Hacker");
+                Console.WriteLine("2.Lockspecialist");
+                Console.WriteLine("3.Muscle");
+                int specialty = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Choose percentage");
+                int percentage = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("choose skill level");
+                int skillLevel = Int32.Parse(Console.ReadLine());
+
+                string userSelect = Console.ReadLine();
+
+                if (userSelect == "1")
+                {
+                    var hacker = new Hacker()
+                    {
+                    Name = name,
+                    SkillLevel = skillLevel,
+                    PercentageCut = percentage
+                    };
+                    Console.WriteLine($"Your name is {name}, your specialty is {skillLevel}, you get {percentage} percent.");
+                }
+                else if (userSelect == "2")
+                {
+                    var lockspecialist = new LockSpecialist()
+                    {
+                    Name = name,
+                    SkillLevel = skillLevel,
+                    PercentageCut = percentage
+                    };
+                    Console.WriteLine($"Your name is {name}, your specialty is {skillLevel}, you get {percentage} percent.");
+                }
+                else if (userSelect == "3")
+                {
+                    var muscle = new Muscle()
+                    {
+                    Name = name,
+                    SkillLevel = skillLevel,
+                    PercentageCut = percentage
+                    };
+                    Console.WriteLine($"Your name is {name}, your specialty is {skillLevel}, you get {percentage} percent.");
+                }
+                else if (userSelect == "")
+                {
+                    Console.WriteLine($"Your name is {name}, your specialty is {skillLevel}, you get {percentage} percent.");
+                    break;
+                }
+            }
         }
     }
 }
