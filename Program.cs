c95e66e797ef46342218312717a0929d46d0af2b
Drew Harper <druharper@gmail.com>
2020-06-05 14:13:24 -0500

    drew

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bank_Heist_5
 {
@@ -6,7 +7,44 @@ namespace Bank_Heist_5
     {
         static void Main(string[] args)
         {
+            List<IRobber> rolodex = new List<IRobber>();
 
+            var drew = new Hacker()
+            {
+                Name = "Drew",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
+
+            var Steve = new Hacker()
+            {
+                Name = "Steve",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
+
+            var Shady = new Muscle()
+            {
+                Name = "Shady",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
+
+            var drPhilipew = new LockSpecialist()
+            {
+                Name = "Philip",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
+
+            var Tyler = new Muscle()
+            {
+                Name = "Tyler",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
         }
+
+
     }
 }
